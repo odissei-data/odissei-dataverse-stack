115af4e7f88042616dadb3e58e0b50556ec78b13
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MBP.ia.knaw.local>
2024-07-24 16:23:58 +0200

    Fixes to language_setup script.

--- utils/language_setup.sh
+++ utils/language_setup.sh
@@ -4,7 +4,9 @@ container_name=$1
 docker exec "$container_name" curl http://localhost:8080/api/admin/settings/:Languages -X PUT -d '[{"locale":"en","title":"English"},{"locale":"nl","title":"Nederlands"}]'
 docker exec -it "$container_name" asadmin create-jvm-options '-Ddataverse.lang.directory=/opt/payara/langBundles'
 docker exec "$container_name" mkdir langBundles
-docker cp -R translations/en_US/*.properties "$container_name":/opt/payara
-docker cp -R translations/nl_NL/*.properties "$container_name":/opt/payara
+docker cp translations/en_US/. "$container_name":/opt/payara
+docker cp translations/nl_NL/. "$container_name":/opt/payara
+docker exec -it -u 0 "$container_name" apt-get update
+docker exec -it -u 0 "$container_name" apt-get install -y zip
 docker exec "$container_name" zip languages.zip *.properties
 docker exec "$container_name" curl http://localhost:8080/api/admin/datasetfield/loadpropertyfiles -X POST --upload-file /opt/payara/languages.zip -H "Content-Type: application/zip"
