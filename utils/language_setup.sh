a9c5af56b1cc4c74077ea5b427e3c40b01e8471f
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2024-07-25 10:12:02 +0200

    Added max depth so that only properties files we need get zipped.

--- utils/language_setup.sh
+++ utils/language_setup.sh
@@ -8,5 +8,5 @@ docker cp translations/en_US/. "$container_name":/opt/payara
 docker cp translations/nl_NL/. "$container_name":/opt/payara
 docker exec -it -u 0 "$container_name" apt-get update
 docker exec -it -u 0 "$container_name" apt-get install -y zip
-docker exec "$container_name" sh -c "find . -name '*.properties' | zip languages.zip -@"
+docker exec "$container_name" sh -c "find . -maxdepth 1 -name '*.properties' | zip languages.zip -@"
 docker exec "$container_name" curl http://localhost:8080/api/admin/datasetfield/loadpropertyfiles -X POST --upload-file /opt/payara/languages.zip -H "Content-Type: application/zip"
