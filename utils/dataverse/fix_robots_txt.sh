9b5560edd8a04b400b8eba7f82df0707133a2053
paulboon <paul.boon@dans.knaw.nl>
2025-03-26 15:50:49 +0100

    Run robots fix script from setup and pass container name

--- utils/dataverse/fix_robots_txt.sh
+++ utils/dataverse/fix_robots_txt.sh
@@ -2,11 +2,13 @@
 #
 # This script takes care to fix the robots.txt file in the Dataverse installation.
 
+dataverse_container_name=$1
+
 # Only allow some crawling on production 
 # noting special on the staging server, so we can just use the dev.robots.txt file
 if $HOSTNAME == "odissei.nl"; then
-  docker cp robots-txt/prod.robots.txt dev_dataverse:/opt/payara/deployments/dataverse/robots.txt
+  docker cp robots-txt/prod.robots.txt "$dataverse_container_name":/opt/payara/deployments/dataverse/robots.txt
 else
-  docker cp robots-txt/dev.robots.txt dev_dataverse:/opt/payara/deployments/dataverse/robots.txt
+  docker cp robots-txt/dev.robots.txt "$dataverse_container_name":/opt/payara/deployments/dataverse/robots.txt
 fi 
 
