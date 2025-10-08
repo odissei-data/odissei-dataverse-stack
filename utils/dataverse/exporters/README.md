a44381e6b2c25f37595239f47d98120a73989905
paulboon <paul.boon@dans.knaw.nl>
2025-10-08 11:11:04 +0200

    Refectoring, making that script a bit better

---
+++ utils/dataverse/exporters/README.md
@@ -0,0 +1,11 @@
+Dataverse metadata exporter plugins
+===================================
+
+This directory contains the 'jar' files for the exporter plugins. 
+Currently there is only Croissant. 
+
+Retrieved from the gdcc maven repo:
+
+```
+wget https://repo1.maven.org/maven2/io/gdcc/export/croissant/0.1.5/croissant-0.1.5.jar
+```
--- utils/dataverse/install_exporters.sh
+++ utils/dataverse/install_exporters.sh
@@ -1,18 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env sh
+#
 # This script copies all exporter jar files from utils/dataverse/exporters to the dataverse docker volume
 # so that they are available to the dataverse application.
-# The script must be run from the root of the repository.
+#
 # The 'caller' must restart the dataverse container to load the new exporter(s).
-set -e
 
-# Check that we have dataverse/docker-dev-volumes/app/data
-if [ ! -d "./utils/dataverse/docker-dev-volumes/app/data" ]; then
-  echo "Please run this script from the root of the repository."
+# Run this from the script directory
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+CURRENT_DIR="$(pwd)"
+cd "$SCRIPT_DIR" || exit 1
+
+echo "Installing exporter jar files. Script directory: $SCRIPT_DIR"
+
+# The dataverse docker volumes directory relative to this script
+DOCKER_VOLUMES_DIR=./docker-dev-volumes
+
+# Test if we have the docker volumes directory
+if [ ! -d "$DOCKER_VOLUMES_DIR" ]; then
+  echo "No $DOCKER_VOLUMES_DIR directory; exiting!"
+  # Get back to where we were
+  cd "$CURRENT_DIR" || exit 1
   exit 1
 fi
 
-EXPORTER_DIR_SRC=./utils/dataverse/exporters
-EXPORTER_DIR_DST=./utils/dataverse/docker-dev-volumes/app/data/exporters
+EXPORTER_DIR_SRC=./exporters
+EXPORTER_DIR_DST=$DOCKER_VOLUMES_DIR/app/data/exporters
 
 # Create the destination directory if it doesn't exist
 if [ ! -d "$EXPORTER_DIR_DST" ]; then
@@ -28,3 +40,6 @@ cp $EXPORTER_DIR_SRC/*.jar $EXPORTER_DIR_DST/
 
 # The 'caller' must restart the dataverse container to load the new exporter(s).
 echo "Note that you must restart the dataverse container to load the new exporter(s)."
+
+# Get back to where we were
+cd "$CURRENT_DIR" || exit 1
