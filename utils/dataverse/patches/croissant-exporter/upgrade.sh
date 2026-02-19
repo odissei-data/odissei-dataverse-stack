0da0343813f641d830a45a8f05b31238ddfbb39c
paulboon <paul.boon@dans.knaw.nl>
2026-02-19 10:07:10 +0100

    Improved croissant upgrade script

--- utils/dataverse/patches/croissant-exporter/upgrade.sh
+++ utils/dataverse/patches/croissant-exporter/upgrade.sh
@@ -12,6 +12,8 @@
 # utils/dataverse/patches/croissant-exporter/upgrade.sh dev_dataverse
 # 
 
+set -e
+
 # Get the dataverse container name from the parameter and, if not given, from the environment variable 
 if [ -n "$1" ]; then
     DATAVERSE_CONTAINER="$1"
@@ -28,6 +30,15 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 CURRENT_DIR="$(pwd)"
 cd "$SCRIPT_DIR" || exit 1
 
+# function to exit with message
+exit_with_error_message() {
+    local message="$1"
+    echo "$message"
+    # Get back to where we were
+    cd "$CURRENT_DIR" || exit 1
+    exit 1
+}
+
 echo "Installing new exporter jar. Script directory: $SCRIPT_DIR"
 
 # The dataverse docker volumes directory relative to this script
@@ -35,14 +46,14 @@ DOCKER_VOLUMES_DIR=../../../../dataverse/docker-dev-volumes
 
 # Test if we have the docker volumes directory
 if [ ! -d "$DOCKER_VOLUMES_DIR" ]; then
-  exit_with_message "No $DOCKER_VOLUMES_DIR directory; exiting!"
+  exit_with_error_message "No $DOCKER_VOLUMES_DIR directory; exiting!"
 fi
 
 EXPORTER_DIR_DST=$DOCKER_VOLUMES_DIR/app/data/exporters
 
 # Check if the exporter directory exist, this is an upgrade; it should be there
 if [ ! -d "$EXPORTER_DIR_DST" ]; then
-  exit_with_message "No $EXPORTER_DIR_DST directory; exiting!"
+  exit_with_error_message "No $EXPORTER_DIR_DST directory; exiting!"
 fi
 
 # Detect an old jar file and rename it to avoid it is loaded by dataverse. 
@@ -58,12 +69,11 @@ fi
 # Copy the jar files from src to dst
 NEW_JAR_FILE=./croissant-0.1.6.jar
 if [ ! -f "$NEW_JAR_FILE" ]; then
-  echo "No $NEW_JAR_FILE file; exiting!"
-  exit_with_message "No $NEW_JAR_FILE file; exiting!"
+  exit_with_error_message "No $NEW_JAR_FILE file; exiting!"
 fi
 
 echo "Copying $NEW_JAR_FILE file to $EXPORTER_DIR_DST"
-cp "$NEW_JAR_FILE" $EXPORTER_DIR_DST/
+cp "$NEW_JAR_FILE" "$EXPORTER_DIR_DST/"
 
 # You must restart the dataverse container to load the new exporter(s).
 echo "Restarting dataverse container to load the new exporter(s)..."
@@ -83,17 +93,17 @@ wait_for_dataverse_up() {
             retries=$((retries + 1))
             if [ "$retries" -ge "$max_retries" ]; then
                 echo "Max retries ($max_retries) reached. Dataverse API is still not up."
-                exit_with_message "Dataverse API did not become responsive in time; exiting!"
+                exit_with_error_message "Dataverse API did not become responsive in time; exiting!"
             fi
             echo "Trial $retries of $max_retries: Still waiting... (HTTP $VERSION_STATUS_CODE)"
             sleep 5
         fi
     done
 }
-# wait for Dataverse to be ready
+
 echo "Waiting for dataverse container to have a responsive API before initiating reExportAll..."
 wait_for_dataverse_up 50
-# note that it might have failed to come up within the time limit
+
 
 echo "Initiating reExportAll to update the export format for the new exporter..."
 STATUS_CODE_RE_EXPORT_ALL=$(docker exec "$DATAVERSE_CONTAINER" curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/api/admin/metadata/reExportAll)
@@ -104,13 +114,7 @@ else
     echo "Dataverse reExportAll did not start: (HTTP $STATUS_CODE_RE_EXPORT_ALL)"
 fi
 
-# function to exit with message
-exit_with_message() {
-    local message="$1"
-    echo "$message"
-    # Get back to where we were
-    cd "$CURRENT_DIR" || exit 1
-    exit 1
-}
-
-exit_with_message "Upgrade complete! Please check the dataverse logs to monitor the reExportAll process. Note that it may take a while for all exports to be updated."
+echo "Upgrade complete! Please check the dataverse logs to monitor the reExportAll process. Note that it may take a while for all exports to be updated."
+# Get back to where we were
+cd "$CURRENT_DIR" || exit 1
+exit 0
