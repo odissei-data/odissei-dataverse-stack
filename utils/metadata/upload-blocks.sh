d718e228b43db4d25454b4624d764a4fa332fdbc
paulboon <paul.boon@dans.knaw.nl>
2026-03-10 09:24:02 +0100

    Update submodules to latest

--- utils/metadata/upload-blocks.sh
+++ utils/metadata/upload-blocks.sh
@@ -2,7 +2,7 @@ echo "Uploading..."
 curl http://localhost:8080/api/admin/datasetfield/load -H "Content-type: text/tab-separated-values" -X POST --upload-file questionInformation.tsv
 curl http://localhost:8080/api/admin/datasetfield/load -H "Content-type: text/tab-separated-values" -X POST --upload-file variableInformation.tsv
 curl http://localhost:8080/api/admin/datasetfield/load -H "Content-type: text/tab-separated-values" -X POST --upload-file CBSMetadata.tsv
-curl http://localhost:8080/api/admin/datasetfield/load -H "Content-type: text/tab-separated-values" -X POST --upload-file DANSmetadata.tsv
+curl http://localhost:8080/api/admin/datasetfield/load -H "Content-type: text/tab-separated-values" -X POST --upload-file dansMetadata.tsv
 curl http://localhost:8080/api/admin/datasetfield/load -H "Content-type: text/tab-separated-values" -X POST --upload-file enrichments.tsv
 curl http://localhost:8080/api/admin/datasetfield/load -H "Content-type: text/tab-separated-values" -X POST --upload-file provenance.tsv
 echo "Done Uploading."
