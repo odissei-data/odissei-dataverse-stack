50e9de8184c77a57c51b1d20b2e67ca37b0cbd1c
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2026-03-26 09:29:12 +0100

    fix: resolved issue with robots.txt and copying it
    The issue was the upgrade script would look for the hostname odissei.nl
    while the production name is odissei01. There were also some issue with
    the robots.txt file such as typos, duplicate lines etc.

--- utils/dataverse/fix_robots_txt.sh
+++ utils/dataverse/fix_robots_txt.sh
@@ -21,7 +21,7 @@ cd "$SCRIPT_DIR" || exit 1
 
 # Only allow some crawling on production 
 # Nothing special on the staging server, so we can just use the dev.robots.txt file
-if [ "$HOSTNAME" = "odissei.nl" ]; then
+if [ "$HOSTNAME" = "odissei01" ]; then
   echo "Copying production robots.txt file to dataverse container"
   docker cp robots-txt/prod.robots.txt "$dataverse_container_name":/opt/payara/deployments/dataverse/robots.txt
 else
